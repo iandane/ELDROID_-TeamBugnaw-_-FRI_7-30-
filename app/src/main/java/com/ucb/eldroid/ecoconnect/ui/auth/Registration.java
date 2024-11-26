8b3119e6f2ee8a6bc15559e8ad4b6a2130c8e815
kimu <rabagokim26@gmail.com>
2024-11-26 20:37:53 +0800

    rabago commit

---
+++ app/src/main/java/com/ucb/eldroid/ecoconnect/ui/auth/Registration.java
@@ -0,0 +1,17 @@
+package com.ucb.eldroid.ecoconnect.ui.auth;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ucb.eldroid.ecoconnect.R;
+
+public class Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+    }
+}
---
+++ app/src/main/java/com/ucb/eldroid/ecoconnect/ui/bottomnav/dashboard/crowdfunding/CreateProject.java
@@ -0,0 +1,21 @@
+package com.ucb.eldroid.ecoconnect.ui.bottomnav.dashboard.crowdfunding;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.ucb.eldroid.ecoconnect.R;
+
+public class CreateProject extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_project);
+
+    }
+}
