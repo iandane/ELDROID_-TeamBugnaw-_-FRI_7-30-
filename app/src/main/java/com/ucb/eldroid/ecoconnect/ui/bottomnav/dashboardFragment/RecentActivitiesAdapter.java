cce24843e8e055cd02d6d2b068a844d8cca87924
Kurtz Malang <kurtz.malang@gmail.com>
2024-11-27 20:34:19 +0800

    malangs

---
+++ app/src/main/java/com/ucb/eldroid/ecoconnect/ui/bottomnav/dashboardFragment/RecentActivitiesAdapter.java
@@ -0,0 +1,4 @@
+package com.ucb.eldroid.ecoconnect.ui.bottomnav.dashboardFragment;
+
+public class RecentActivitiesAdapter {
+}
---
+++ app/src/main/java/com/ucb/eldroid/ecoconnect/ui/bottomnav/environmentalData/EnvironmentalDataAdapter.java
@@ -0,0 +1,4 @@
+package com.ucb.eldroid.ecoconnect.ui.bottomnav.environmentalData;
+
+public class EnvironmentalDataAdapter {
+}
