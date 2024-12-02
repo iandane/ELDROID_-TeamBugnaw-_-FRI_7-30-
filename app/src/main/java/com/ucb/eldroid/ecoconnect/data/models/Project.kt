dbc814330284dd8414b0014969aaf54bdf744365
kimu <rabagokim26@gmail.com>
2024-12-02 15:22:04 +0800

    rabago7

--- app/src/main/java/com/ucb/eldroid/ecoconnect/data/models/Project.kt
+++ app/src/main/java/com/ucb/eldroid/ecoconnect/data/models/Project.kt
@@ -1,3 +1,10 @@
 package com.ucb.eldroid.ecoconnect.data.models
 
-class Project
+class Project(
+    val projectId: String? = null,   // Nullable projectId, will be assigned later
+    val title: String,
+    val description: String,
+    val goalAmount: Double,
+    val deadline: String,
+    val imagePath: String?
+)
