cf8b9e805535abe4e3a2b43f42fac4424101f166
Loys <wagasloys@gmail.com>
2024-12-04 00:55:05 +0800

    serverss

--- app/src/main/java/com/ucb/eldroid/ecoconnect/utils/RetrofitClient.kt
+++ app/src/main/java/com/ucb/eldroid/ecoconnect/utils/RetrofitClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     private var retrofit: Retrofit? = null
-    private const val BASE_URL = "http://192.168.1.18:8000/api/"
+    private const val BASE_URL = "http://10.0.0.33:8000/api/"
 
     @JvmStatic
     val instance: Retrofit?
